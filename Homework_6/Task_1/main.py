4eff139a5a4d98484269baffa7c2941ec1d366e4
Мария Жигар <maria.zhigar@gmail.com>
2026-08-21 17:37:47 +0300

    Homework_6_Python

---
+++ Homework_6/Task_1/main.py
@@ -0,0 +1,3 @@
+# Task 1: Greeting
+name = input("Как тебя зовут?\n")
+print(f"Привет, {name}! Приятно познакомиться") # f-lines
---
+++ Homework_6/Task_2/main.py
@@ -0,0 +1,4 @@
+# Task 2: Rectangle area
+length = float(input("Введите длину прямоугольника: "))
+width = float(input("Введите ширину прямоугольника: "))
+print(f"Площадь прямоугольника: {length * width}") # f-string
